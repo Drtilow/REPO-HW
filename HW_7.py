7b6c97cd54d42b3a07dee39b7f71add8d82facc3
davidSetek <neco@seznam.cz>
2025-03-10 16:07:00 +0100

    jak bude vypadat program

---
+++ HW_7.py
@@ -0,0 +1,15 @@
+# Seznam na uložení záznamů
+
+# Hlavní smyčka aplikace
+
+    # Zobrazení menu
+
+    # Přidání nového záznamu
+
+    # Zobrazení existujících záznamů
+
+    # Vyhledání záznamu podle klíčového slova
+
+    # Ukončení aplikace
+
+    # Neplatná volba
