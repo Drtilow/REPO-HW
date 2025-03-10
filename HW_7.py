4847db2161c6b180ec214f01004c3bb3d2cc0006
davidSetek <neco@seznam.cz>
2025-03-10 16:08:53 +0100

    Update HW_7.py
    zobrazení menu

--- HW_7.py
+++ HW_7.py
@@ -1,9 +1,16 @@
+import sys
 # Seznam na uložení záznamů
-
+zaznamy = []
 # Hlavní smyčka aplikace
-
+while True:
     # Zobrazení menu
-
+    print("\nMenu:")
+    print("1. Přidat nový záznam")
+    print("2. Zobrazit existující záznamy")
+    print("3. Vyhledat záznam podle klíčového slova")
+    print("4. Ukončit aplikaci")
+    volba = input("Vyberte možnost: ")
+    
     # Přidání nového záznamu
 
     # Zobrazení existujících záznamů
